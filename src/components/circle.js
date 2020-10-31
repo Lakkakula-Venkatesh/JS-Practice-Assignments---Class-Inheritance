cd5b1cced694e5a945bf98a62416e3c477184945
Lakkakula-Venkatesh <68702624+Lakkakula-Venkatesh@users.noreply.github.com>
2020-10-31 21:53:36 +0530

    Changes

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,11 @@
 // import using require
-
+import Shape from "./shape";
 // declare class
-
+export default class Circle extends Shape {
+  constructor(color) {
+    super(color);
+  }
+  calculateArea = function() {}
+}
 
 // export class using module.exports
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,11 @@
 // declare class
+export default class Shape {
+  color = null;
 
+  constructor(color) {
+    this.color = color;
+  }
+  drawShape = function() {};
+  calculateArea = function() {};
+}
 // export class using module.exports
