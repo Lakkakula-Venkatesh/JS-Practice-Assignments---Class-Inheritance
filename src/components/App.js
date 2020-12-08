2825c37318531d16ca5aa4458689af7ce0d73947
Lakkakula-Venkatesh <68702624+Lakkakula-Venkatesh@users.noreply.github.com>
2020-12-08 21:55:45 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
-import Circle from "./Circle";
-import Shape from "./Shape";
+const Circle =  require("./Circle");
+const Shape = require("./Shape");
 class App extends Component {
   render() {
     return (
