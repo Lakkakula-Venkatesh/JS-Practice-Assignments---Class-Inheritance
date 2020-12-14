a2237f59a414534953c097ee74df9ad4dd5b13a0
Lakkakula Venkatesh <lakkakula.venkatesh8726@gmail.com>
2020-12-14 10:25:28 +0530

    New Changes

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,13 @@
 // import using require
-
+let Shape = require("./shape");
 // declare class
-
+class Circle extends Shape {
+    constructor() {
+      super();
+      this.color = "blue";
+    }
+    calculateArea() {}
+  }
+  module.exports = Circle;
 
 // export class using module.exports
